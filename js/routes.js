3499995039c36d74e48abef945f011fad2d8028c
Saad Mir <saadmir@gmail.com>
2026-08-27 09:15:56 -0700

    added alltrails embeds

---
+++ js/routes.js
@@ -0,0 +1,19 @@
+/**
+ * K2 Gallery -- Daily Trek Route Data (AllTrails embeds)
+ *
+ * Edit the  title  field to rename a day's route.
+ * embed = the AllTrails widget URL from the <iframe src="...">.
+ */
+
+const ROUTES = [
+  { day: 1, title: "(02/Aug) Askole → Jhola", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-askole-jhola-e292964?u=i&sh=fbrpsk" },
+  { day: 2, title: "(03/Aug) Jhola → Paiju", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-jhola-paiju-4ed8574?u=i&sh=fbrpsk" },
+  { day: 3, title: "(05/Aug)Paiju → Khoburtse", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-paiju-khoburtse-f375347?u=i&sh=fbrpsk" },
+  { day: 4, title: "(06/Aug) Khoburtse → Urdukas", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-khoburtse-urdukas-6446bef?u=i&sh=fbrpsk" },
+  { day: 5, title: "(07/Aug) Urdukas → Goro II", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-urdukas-goro-ii-c4f3424?u=i&sh=fbrpsk" },
+  { day: 6, title: "(08/Aug) Goro II - Conordia", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-goro-ii-concordia-043deb5?u=i&sh=fbrpsk"},
+  { day: 7, title: "(10/Aug)Concordia → Ali Camp", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-gondogoro-la-variant-concordia-ali-camp-93c0a55?u=i&sh=fbrpsk" },
+  { day: 8, title: "(11/Aug, 12/Aug) Ali Camp -> Gondogoro La/Khuspang", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-gondogoro-la-variant-ali-camp-gondogoro-la-khuspang-c9b48d8?u=i&sh=fbrpsk" },
+  { day: 9, title: "(12/Aug) Khuspang → Saicho", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-gondogoro-la-variant-khuspang-saicho-3186c80?u=i&sh=fbrpsk" },
+  { day: 10, title: "(13/Aug) Saicho → Hushe", embed: "https://www.alltrails.com/widget/recording/k2-base-camp-trek-gondogoro-la-variant-saicho-hushe-4d71138?u=i&sh=fbrpsk" }
+];
